315a82885923da02c0b41fbf3d97577da59e7bc2
anatolij <anatolij001@gmail.com>
2024-03-19 12:18:47 +0300

    Initial Commit.

---
+++ src/main/java/ru/sidorovan/HW/_9/Libraries/Hw29LibrariesApplication.java
@@ -0,0 +1,13 @@
+package ru.sidorovan.HW._9.Libraries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw29LibrariesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw29LibrariesApplication.class, args);
+	}
+
+}
